c09106c7884f916667a8689cfe5a1999b743824e
Ellie <kristaelizabethghazaleh@gmail.com>
2026-02-12 22:35:16 -0500

    Update test_steffenson.m

--- test_steffenson/test_steffenson.m
+++ test_steffenson/test_steffenson.m
@@ -21,17 +21,23 @@ for idx = 1:numel(start_values)
     fprintf('root_approx = %.16e\n', root_approx);
     fprintf('f(root_approx) = %.3e\n', f_at_root);
     fprintf('iterations = %d\n', iteration_count);
+
+    if abs(f_at_root) > abs_tol
+        fprintf('WARNING: did not converge to tolerance (try a different start).\n');
+    end
 end
 
 %% Problem 2: first ten positive roots of tan(x)=x
-% Safer initial guess: just to the right of n*pi (stays away from asymptote)
+% Better initial guess: just left of the asymptote, but not too close
+% x_n ~ (n+1/2)pi - 1/((n+1/2)pi)
 root_count = 10;
 roots = zeros(root_count, 1);
 
 fprintf('\n\nFirst ten positive roots of tan(x) = x:\n');
 
 for n = 1:root_count
-    start_x = n*pi + 0.1;   % safe starting point inside (n*pi, (n+1/2)pi)
+    asymptote_x = (n + 0.5) * pi;
+    start_x = asymptote_x - 1/asymptote_x;   % good start inside (n*pi, (n+1/2)pi)
 
     [root_approx, f_at_root, iteration_count] = steffensen_method( ...
         f_handle, start_x, abs_tol, max_iters);
@@ -40,6 +46,10 @@ for n = 1:root_count
 
     fprintf('n=%2d  start_x=%.12f  root=%.16f  f(root)=%.3e  iters=%d\n', ...
         n, start_x, root_approx, f_at_root, iteration_count);
+
+    if abs(f_at_root) > abs_tol
+        fprintf('  WARNING: did not converge to tolerance for n=%d.\n', n);
+    end
 end
 
 fprintf('\nroots = \n');
